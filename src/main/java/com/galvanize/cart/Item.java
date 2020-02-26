07cfbffdc0500a2c6e33668dff983aab059ba078
Monica Howard <mo@Monicas-MacBook-Air.local>
2020-02-26 15:42:40 -0600

    adds basic Item class

--- src/main/java/com/galvanize/cart/Item.java
+++ src/main/java/com/galvanize/cart/Item.java
@@ -1,4 +1,30 @@
 package com.galvanize.cart;
 
 public class Item {
+    //properties
+    String itemName;
+    double itemPrice;
+
+    //constructor
+    public Item(String name, double itemPrice){
+        this.itemName = name;
+        this.itemPrice = itemPrice;
+    }
+
+    //methods
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public double getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(double itemPrice) {
+        this.itemPrice = itemPrice;
+    }
 }
