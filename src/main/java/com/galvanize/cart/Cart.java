a4c094cced7784ec2440d7fdd4d8f59f14933be7
Monica Howard <mo@Monicas-MacBook-Air.local>
2020-02-26 16:17:49 -0600

    created AddItem method in Cart class

--- src/main/java/com/galvanize/cart/Cart.java
+++ src/main/java/com/galvanize/cart/Cart.java
@@ -1,4 +1,37 @@
 package com.galvanize.cart;
 
+import java.util.ArrayList;
+
 public class Cart {
+    //properties
+    ArrayList<Item> itemsInCart = new ArrayList<Item>();
+
+    //no constructor since we want cart to initialize as EMPTY
+
+    // methods [ getTotalPrice() - itemizedList() - getTotalPrice() - itemQuantities() - onSaleItems() ]
+
+    public void AddItem(Item item){
+        itemsInCart.add(item);
+    }
+
+    public void AddItem(Item item, int quantity){
+
+    }
+
+    public void getTotalPrice(){
+
+    }
+
+    public void itemizedList(){
+
+    }
+
+
+    public void itemQuantities(){
+
+    }
+
+    public void onSaleItems(){
+
+    }
 }
